07bb2be4fbe17f6a8384ae0283b524e857d2fe78
martinchiu <martin87713@gmail.com>
2022-01-08 13:45:38 +0800

    feat: 新增計算總金額功能

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,13 +7,16 @@ const CATEGORY = require('../../models/seeds/category.json').results
 
 // 首頁
 router.get('/', (req, res) => {
+  let totalAmount = 0
+
   Record.find()
     .lean()
     .then(records => {
       records.forEach(record => {
         record.icon = CATEGORY.find(category => category.name === record.category).icon
+        totalAmount += record.amount
       })
-      res.render('index', { records })
+      res.render('index', { records, totalAmount})
     })
     .catch(error => console.error(error))
 })
