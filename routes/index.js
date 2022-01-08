f27bea60ad684e5465a5c606e791a6124a5a36a2
martinchiu <martin87713@gmail.com>
2022-01-08 16:34:40 +0800

    feat: 新增驗證登入機制

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
+const { authenticator } = require('../middleware/auth')
 const records = require('./modules/records')
 const home = require('./modules/home')
 const users = require('./modules/users')
 
 router.use('/users', users)
-router.use('/records', records)
-router.use('/', home)
+router.use('/records', authenticator, records)
+router.use('/', authenticator, home)
 
 module.exports = router
