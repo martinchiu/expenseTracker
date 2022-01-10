3066dff294242cf43e4573ebe2222dcc10efe98c
martinchiu <martin87713@gmail.com>
2022-01-10 21:29:22 +0800

    feat: 重構種子資料

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,41 +1,55 @@
+const bcrypt = require('bcryptjs')
 const db = require('../../config/mongoose')
 
 const Record = require('../record')
 const Category = require('../category')
-const category = require('../category')
-const record = require('../record')
+const User = require('../user')
 
 const CATEGORY = require('./category.json').results
+const userSeedData = require('./userSeed.json').results
 
 db.once('open', () => {
-  // 新增 record 種子資料
+  Category.find()
+    .lean()
+    .then(categories => {
+      return Promise.all(Array.from(
+        CATEGORY,
+        (value) => {
+          value.categoryId = categories.find(category => category.name === value.name)._id
+        }
+      ))
+    })
   Promise.all(Array.from(
-    CATEGORY,
-    (value, index) => {
-      Record.create({
-        name: 'name-' + index,
-        date: Date.now(),
-        amount: index,
-        category: value.name
+    userSeedData,
+    (user) => {
+     return bcrypt.genSalt(10)
+      .then(salt => { return bcrypt.hash(user.password, salt) })
+      .then(hash => {
+        return User.create({
+          name: user.name,
+          email: user.email,
+          password: hash
+        })
       })
-    }
-  ))
-  return Category.find()
-  .lean()
-  .then(categories => {
-    // 讓 record 種子資料跟 category 種子資料關聯
-    Promise.all(Array.from(
-      categories,
-      (value) => {
-        Record.findOne({ category: value.name })
-          .then(record => {
-            record.categoryId = value._id
-            record.save()
-          })
-      }
-    ))
-      .then(() => {
-        console.log('recordSeeder done.')
-      })  
-  })
+      .then(registeredUser => {
+        const userId = registeredUser._id
+        return Promise.all(Array.from(
+          CATEGORY,
+          (value, index) => {
+            return Record.create({
+              name: 'name-' + index,
+              date: Date.now(),
+              amount: index,
+              category: value.name,
+              categoryId: value.categoryId,
+              userId
+            })
+          }
+        ))
+      })
+  }))
+    .then(() => {
+      console.log('recordSeeder done.')
+      process.exit()
+    })
 })
