cfa8747a7c3056ec0bf8942e25c2a9c1925ff476
martinchiu <martin87713@gmail.com>
2022-01-13 12:20:33 +0800

    feat: 在mongoose.js 內帶入.env 變數

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,4 +1,7 @@
-const mongoose = require('mongoose') 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI)
 
 
