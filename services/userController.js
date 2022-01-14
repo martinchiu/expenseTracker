162f0932f6b12ec2252320ee0a6021486debd08a
martinchiu <martin87713@gmail.com>
2022-01-14 18:27:16 +0800

    refactor: 用async/await語法重構註冊路由

--- services/userController.js
+++ services/userController.js
@@ -16,7 +16,7 @@ module.exports = {
   register: (req, res) => {
     res.render('register')
   },
-  postRegister: (req, res) => {
+  postRegister: async (req, res) => {
     const { name, email, password, confirmPassword } = req.body
     const errors = []
     if (!name || !email || !password || !confirmPassword) {
@@ -34,28 +34,30 @@ module.exports = {
         confirmPassword
       })
     }
-    User.findOne({ email }).then(user => {
-      // 如果已經註冊：退回註冊頁
-      if (user) {
-        errors.push({ message: '這個 Email 已經註冊過了。' })
-        res.render('register', {
-          errors,
-          name,
-          email,
-          password,
-          confirmPassword
-        })
-      }
-      return bcrypt.genSalt(10)
-        .then(salt => bcrypt.hash(password, salt))
-        .then(hash => User.create({
-          name,
-          email,
-          password: hash
-        }))
-        .then(() => res.render('login', { email }))
-        .catch(err => console.log(err))
+    const user = await User.findOne({ email })
+    // 如果已經註冊：退回註冊頁
+    if (user) {
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+    await User.create({
+      name,
+      email,
+      password: hash
     })
+    try {
+      res.render('login', { email })
+    } catch (err) {
+      console.log(err)
+    }
   },
   // 登出
   logout: (req, res) => {
